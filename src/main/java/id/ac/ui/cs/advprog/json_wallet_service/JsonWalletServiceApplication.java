bd5dfcf0a9fdb191080e7240632bef89a32f3953
jebs-tech <bungasaragih688@gmail.com>
2026-02-20 12:09:07 +0700

    chore: initial Spring Boot setup

---
+++ src/main/java/id/ac/ui/cs/advprog/json_wallet_service/JsonWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.json_wallet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWalletServiceApplication.class, args);
+	}
+
+}
