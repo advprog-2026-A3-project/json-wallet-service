fae19a90d9a72b9552301a633d3705e59c592cbb
jebs-tech <bungasaragih688@gmail.com>
2026-03-03 20:40:31 +0700

    chore: remove accidentally committed ADPRO folder

--- ADPRO/wallet-service/src/main/java/com/json/demo/repository/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.json.demo.repository;
-
-import com.json.demo.model.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface WalletRepository extends JpaRepository<Wallet, String> {
-    Optional<Wallet> findByUserId(String userId);
-}
