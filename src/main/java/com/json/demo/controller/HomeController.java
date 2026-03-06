80b662e5567f6cda48c570526b23db388ddb0b18
jebs-tech <bungasaragih688@gmail.com>
2026-03-06 21:16:22 +0700

    feat: add home redirect to wallet dashboard

---
+++ src/main/java/com/json/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.json.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/view/wallet/test123";
+    }
+} 
